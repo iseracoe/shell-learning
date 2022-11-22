14de506e94715efeb2f24d6c013170e73770c19b
md-khan07 <skhan1133i@gmail.com>
2022-11-22 23:05:32 +0530

    Adding array,file_op,global var, local var, var_code shell files

---
+++ rahmath/var_code.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Author : Md
+
+echo "First Name: $0"
+echo "First parameter: $1"
+echo "Second Parameter: $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters: $#"
