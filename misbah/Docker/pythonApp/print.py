eda7552a443797900fa69e531227417301d163b9
misbah2305 <misbahkhaja23@gmail.com>
2022-12-14 11:18:20 -0500

    Docker files

---
+++ misbah/Docker/pythonApp/print.py
@@ -0,0 +1,2 @@
+
+print ("Hello world,My First Docker image!!")
--- misbah/case2.sh
+++ misbah/case2.sh
@@ -9,10 +9,10 @@ echo "green"
 echo "orange"
 read color;
 case $color in
-	Blue echo "blue is a primary color.";;
-	Red echo "Red is a primary color.";;
-	Yellow echo "Yellow is a primary color.";;
-	green echo "green is a primary color.";;
-	orange echo "orange is a primary color.";;
+	Blue) echo "blue is a primary color.";;
+	Red) echo "Red is a primary color.";;
+	Yellow) echo "Yellow is a primary color.";;
+	green) echo "green is a primary color.";;
+	orange) echo "orange is a primary color.";;
 	*) echo "this color is not available. please choose a different one.";;
 esac	
